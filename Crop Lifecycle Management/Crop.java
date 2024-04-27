ed968095f47a741835fc787917666e90a1bd4921
indigopaku <108289555+indigopaku@users.noreply.github.com>
2024-04-27 14:17:54 -0700

    Update Crop.java

--- Crop Lifecycle Management/Crop.java
+++ Crop Lifecycle Management/Crop.java
@@ -1,60 +1,35 @@
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
-// Crop class to represent individual crops
 public class Crop {
     private String name;
-    private int expirationDaysFromPlanting; // Duration in days from planting
-    private LocalDate plantingDate;
-    private LocalDate harvestDate;
+    private int expirationDuration; // in days
+    private List<Task> tasks;
 
-    public Crop(String name, int expirationDaysFromPlanting) {
+    public Crop(String name, int expirationDuration) {
         this.name = name;
-        this.expirationDaysFromPlanting = expirationDaysFromPlanting;
+        this.expirationDuration = expirationDuration;
+        this.tasks = new ArrayList<>();
     }
 
-    // Getters and setters
     public String getName() {
         return name;
     }
 
-    public int getExpirationDaysFromPlanting() {
-        return expirationDaysFromPlanting;
-    }
-    /*added planting date,harvesting date*/
-    public LocalDate getPlantingDate() {
-        return plantingDate;
+    public int getExpirationDuration() {
+        return expirationDuration;
     }
 
-    public void setPlantingDate(LocalDate plantingDate) {
-        this.plantingDate = plantingDate;
+    public List<Task> getTasks() {
+        return tasks;
     }
 
-    public LocalDate getHarvestDate() {
-        return harvestDate;
+    public void addTask(Task task) {
+        tasks.add(task);
     }
 
-    public void setHarvestDate(LocalDate harvestDate) {
-        this.harvestDate = harvestDate;
-    }
-
-    public LocalDate calculateExpirationDate() {
-        if (plantingDate != null) {
-            return plantingDate.plusDays(expirationDaysFromPlanting);
-        } else {
-            return null;
-        }
-    }
-
-    public String formatDate(LocalDate date) {
-        if (date != null) {
-            return date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        } else {
-            return "Not set";
-        }
-    }
     @Override
     public String toString() {
-        return "Crop: " + name + " | Expiration: " + expirationDaysFromPlanting + " days from planting";
+        return "Crop: " + name + " | Expiration: " + expirationDuration + " days from planting";
     }
 }
