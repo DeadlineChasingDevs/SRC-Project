0796c8524ccfdcf39bfef79b6f06f1dc53404fe0
lebuckman <liambuckman0@gmail.com>
2024-04-27 15:53:40 -0700

    No-arg constructor, display method, and add crop method

--- src/CropManager.java
+++ src/CropManager.java
@@ -3,6 +3,10 @@ import java.util.List;
 public class CropManager {
     private List<Crop> crops;
 
+    public CropManager() {
+        this(new ArrayList<>());
+    }
+
     public CropManager(List<Crop> crops) {
         this.crops = crops;
     }
@@ -13,6 +17,16 @@ public class CropManager {
             System.out.println(crop);
         }
     }
+
+    public void addCrop(Crop crop) {
+        crops.add(crop);
+    }
+    
+    public void displayCrops() {
+        System.out.println("Crop List:");
+
+        for (Crop crop : crops) {
+            System.out.println(crop);
+        }
+    }
 }
-
-
