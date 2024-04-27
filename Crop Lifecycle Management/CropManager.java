6ab3252e4c7148b0218b56e5db3e066f4dc5d5bd
indigopaku <108289555+indigopaku@users.noreply.github.com>
2024-04-27 15:33:26 -0700

    Update CropManager.java

--- Crop Lifecycle Management/CropManager.java
+++ Crop Lifecycle Management/CropManager.java
@@ -1,19 +1,18 @@
-import java.util.ArrayList;
 import java.util.List;
 
 public class CropManager {
     private List<Crop> crops;
 
-    public CropManager() {
-        this.crops = new ArrayList<>();
+    public CropManager(List<Crop> crops) {
+        this.crops = crops;
     }
 
-    public List<Crop> getCrops() {
-        return crops;
-    }
-
-    public void addCrop(Crop crop) {
-        crops.add(crop);
+    public void displayCrops() {
+        System.out.println("List of Crops:");
+        for (Crop crop : crops) {
+            System.out.println(crop);
+        }
     }
 }
+
 
