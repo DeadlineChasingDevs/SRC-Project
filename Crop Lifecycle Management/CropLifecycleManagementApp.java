ca2c6f277fd8ba3928241ce40feda4fb9c003b5d
indigopaku <108289555+indigopaku@users.noreply.github.com>
2024-04-26 23:21:18 -0700

    Create CropLifecycleManagementApp.java

---
+++ Crop Lifecycle Management/CropLifecycleManagementApp.java
@@ -0,0 +1,24 @@
+// Main class to demonstrate the functionality of the Crop Lifecycle Management section
+public class CropLifecycleManagementApp {
+    public static void main(String[] args) {
+        // Instantiate CropManager
+        CropManager cropManager = new CropManager();
+
+        // Add crops
+        Crop crop1 = new Crop("Tomatoes", 60); // Expiration: 60 days from planting
+        Crop crop2 = new Crop("Lettuce", 90); // Expiration: 90 days from planting
+        Crop crop3 = new Crop("Carrots", 120); // Expiration: 120 days from planting
+        Crop crop4 = new Crop("Broccoli", 75); // Expiration: 75 days from planting
+        Crop crop5 = new Crop("Bell Peppers", 80); // Expiration: 80 days from planting
+
+        // Add crops to CropManager
+        cropManager.addCrop(crop1);
+        cropManager.addCrop(crop2);
+        cropManager.addCrop(crop3);
+        cropManager.addCrop(crop4);
+        cropManager.addCrop(crop5);
+
+        // Display the list of crops
+        cropManager.displayCrops();
+    }
+}
