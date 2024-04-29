981369ae3dbf42c6cd7b2ca1b54c039dd9ad0298
1hperic <159103466+1hperic@users.noreply.github.com>
2024-04-28 21:30:47 -0700

    added javadoc comments

--- src/InventoryAnalytics.java
+++ src/InventoryAnalytics.java
@@ -1,43 +1,83 @@
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class InventoryAnalytics {
 	private double revenue;
 	private double expenses;
 	private List<Product> inventory;
 
+	/**
+	 * Constructor creates new inventory.
+	 */
 	public InventoryAnalytics() {
 		this.inventory = new ArrayList<>();
 	}
-
+	
+	/**
+	 * Gets total revenue, needs to be set and/or calculated first
+	 * 
+	 * @return
+	 */
 	public double getRevenue() {
 		return revenue;
 	}
 
+	/**
+	 * Calculates profits based on revenue and total expenses.
+	 */
 	public double getProfits() {
 		return revenue - expenses;
 	}
 
+	/**
+	 * Gets expenses on inventory, needs to be set
+	 * 
+	 * @return
+	 */
 	public double getExpenses() {
 		return expenses;
 	}
 
+	/**
+	 * Sets expenses on inventory
+  	 * 
+	 * @return
+	 */
 	public void setExpenses(double expenses) {
 		this.expenses = expenses;
 	}
 
+	/**
+	 * Sets total revenue
+	 * 
+	 * @return
+	 */
 	public void setRevenue(double revenue) {
 		this.revenue = revenue;
 	}
 
+	/**
+	 * Adds a product to ArrayList inventory
+	 * 
+	 * @param product
+	 */
 	public void addProduct(Product product) {
 		inventory.add(product);
 	}
 
+	/**
+	 * Removes a product from ArrayList inventory
+	 * 
+	 * @param product
+	 */
 	public void removeProduct(Product product) {
 		inventory.remove(product);
 	}
 
+	/**
+	 * Prints current inventory. Gives items, number of stock, pricetag.
+	 */
 	public void getInventory() {
 		for (Product product : inventory) {
 			System.out.println("Item: " + product.getName());
@@ -46,13 +86,23 @@ public class InventoryAnalytics {
 			System.out.println();
 		}
 	}
-
+	
+	/**
+	 * Sells a specified number of a specific product.
+	 * Updates appropriate values of that product and adds to revenue.
+	 * 
+	 * @param product
+	 * @param numPurchased
+	 */
 	public void sellItem(Product product, int numPurchased) {
 		product.setQuantity(product.getQuantity() - numPurchased);
 		product.setSold(product.getSold() + numPurchased);
 		revenue += product.getSellPrice();
 	}
 
+	/**
+	 * Calculates value spent on inventory.
+	 */
 	public double getValInventory() {
 		double valInventory = 0.0;
 
@@ -62,6 +112,9 @@ public class InventoryAnalytics {
 		return valInventory;
 	}
 
+	/**
+	 * Calculates turnover rate.
+	 */
 	public double getTurnoverRate() {
 		double soldGoodsValue = 0.0;
 
@@ -71,6 +124,15 @@ public class InventoryAnalytics {
 		return soldGoodsValue / getValInventory(); // turnover rate
 	}
 
+	/**
+	 * Retrieves a product from the inventory by its name.
+	 * 
+	 * This method searches the inventory for a product with the specified name.
+	 * If found, it returns the product; otherwise, it returns null.
+	 *
+	 * @param product
+	 * @return
+	 */
 	public Product getProduct(String product) {
 		int next = 0;
 		boolean found = false;
