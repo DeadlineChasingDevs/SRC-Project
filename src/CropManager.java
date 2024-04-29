50bf8e68a836350fad9cd7e6a9facee60baddf06
indigopaku <108289555+indigopaku@users.noreply.github.com>
2024-04-28 20:06:45 -0700

    JavaDoc comments added

--- src/CropManager.java
+++ src/CropManager.java
@@ -1,21 +1,40 @@
 import java.util.List;
 import java.util.ArrayList;
 
+/**
+ * Manages a list of crops, allowing addition and display of crops.
+ */
 public class CropManager {
     private List<Crop> crops;
 
+    /**
+     * Constructs a CropManager with an empty list of crops.
+     */
     public CropManager() {
         this(new ArrayList<>());
     }
 
+    /**
+     * Constructs a CropManager with the specified list of crops.
+     *
+     * @param crops The list of crops to manage.
+     */
     public CropManager(List<Crop> crops) {
         this.crops = crops;
     }
 
+    /**
+     * Adds a crop to the list of managed crops.
+     *
+     * @param crop The crop to add.
+     */
     public void addCrop(Crop crop) {
         crops.add(crop);
     }
-    
+
+    /**
+     * Displays the list of crops managed by this CropManager.
+     */
     public void displayCrops() {
         System.out.println("Crop List:");
 
@@ -24,7 +43,13 @@ public class CropManager {
         }
     }
 
-    public List<Crop> getCrops(){
+    /**
+     * Gets the list of crops managed by this CropManager.
+     *
+     * @return The list of crops.
+     */
+    public List<Crop> getCrops() {
         return crops;
     }
 }
+
