24a8b1a0c2a060f021fe6ae0e095e42ddf37e009
jazcar1205 <158844693+jazcar1205@users.noreply.github.com>
2024-04-28 20:13:34 -0700

    Update Volunteer.java

--- src/Volunteer.java
+++ src/Volunteer.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+* Class specified for Volunteers
+**/
 public class Volunteer {
     private String name;
     private List<Task> assignedTasks;
